42c0af12d6e3f248ddfa759c333ed84d4f1c7c54
Noah Appelbaum <noahappelbaum@gmail.com>
2023-10-13 14:31:48 -0500

    added 7-day expiration for signed s3 urls

--- scripts/s3_upload.py
+++ scripts/s3_upload.py
@@ -5,26 +5,27 @@ URL_WITH_REGION_CODE = "https://s3.us-west-1.amazonaws.com"
 
 class AWS:
     """Allows user to interact with AWS s3 for files."""
+
     def __init__(self, aws_access_key_id, aws_secret_access_key, bucket_name):
         self.bucket_name = bucket_name
-        self.s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        self.s3_client = boto3.client(
+            's3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     def save_file(self, file, filename):
+        """Uploads file with filename to Amazon s3 bucket"""
         self.s3_client.upload_fileobj(
             file,
             self.bucket_name,
             filename,
             ExtraArgs={"ContentType": "image/jpg"}
-            )
+        )
 
     def get_presigned_url(self, filename):
-        response = self.s3_client.generate_presigned_url('get_object',
-                                                Params={'Bucket': self.bucket_name,
-                                                        'Key': filename})
+        """Retrieves presigned url for filename from s3"""
+        response = self.s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': self.bucket_name,
+                    'Key': filename},
+            ExpiresIn=608700
+        )
         return response
-
-
-# filename = AWS.save_file("chow2.jpg", "chow2.jpg")
-# print("Filename", filename)
-# aws_info = AWS.get_file_info_from_aws(filename)
-# print("AWS Info", aws_info)
